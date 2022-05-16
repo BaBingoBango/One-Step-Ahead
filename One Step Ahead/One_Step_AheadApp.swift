cff92e812628dafea468d221e6cca4d5e4f456f5
Ethan Marshall <ethanm265@yahoo.com>
2022-05-16 16:51:24 -0400

    Initial Commit

---
+++ One Step Ahead/One_Step_AheadApp.swift
@@ -0,0 +1,17 @@
+//
+//  One_Step_AheadApp.swift
+//  One Step Ahead
+//
+//  Created by Ethan Marshall on 5/16/22.
+//
+
+import SwiftUI
+
+@main
+struct One_Step_AheadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
