d79b092b7949872dbf29c44eb4fe16bf4b7282af
Ethan Marshall <ethanm265@yahoo.com>
2022-07-09 01:31:58 -0400

    Re-locked the task views!

--- One Step Ahead/Views/Gallery Views/TaskRectangleView.swift
+++ One Step Ahead/Views/Gallery Views/TaskRectangleView.swift
@@ -38,7 +38,7 @@ struct TaskRectangleView: View {
     var body: some View {
         
         /// Whether or not the task represented by this view is unlocked.
-        let isTaskUnlocked = !userTaskRecords.records.keys.contains(task.object) // FIXME: !
+        let isTaskUnlocked = userTaskRecords.records.keys.contains(task.object)
         
         Button(action: {
             isShowingTaskDetailView = true
